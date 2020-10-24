3475fadf8ea300cd1213558f1f86dc92d900cdb5
Joshua Santos <jsavatar717@gmail.com>
2020-10-24 19:14:54 +0100

    #2
    - Renamed (not final) method/function names
    - Commented Heap related classes
    - Switched up the interpreter
    - Josh

--- HeapPriorityQueue.py
+++ HeapPriorityQueue.py
@@ -2,123 +2,127 @@ class PriorityQueueBase:
     ''' Composition design pattern - assured that each element
         remained paired with its associated count in our
         primary data structure'''
-    class _item:
-        __slots__ = '_key', '_value' #storing items internally as pairs
+    class Item:
+        __slots__ = 'key', 'value'  #storing items internally as pairs
+
         def __init__(self, k, v):
-            self._key = k
-            self._value = v
+            self.key = k
+            self.value = v
 
         def __it__(self, other):
-            return self._key < other._key #compare items based on their keys
+            return self.key < other.key #compare items based on their keys
+
     def is_empty(self):
         return len(self) == 0 #return True if priority queue is empty
 
 class HeapPriorityQueue(PriorityQueueBase):
     ''' NONPUBLIC BEHAVIOURS'''
-    def _parent(self, j):
-        return (j-1)//2
+    def parent(self, j):
+        return (j-1)//2 #Inverse way of plotting children
 
-    def _left(self, j):
-        return (2*j) + 1
+    def left(self, j):
+        return (2*j) + 1 #Plotting Left Child
 
-    def _right(self, j):
-        return (2*j) + 2
+    def right(self, j):
+        return (2*j) + 2 #Plotting right child
 
-    def _has_left(self, j):
-        return self._left(j) < len(self._data)
+    def has_left(self, j):
+        return self.left(j) < len(self.data)
 
-    def _has_right(self, j):
-        return self._right(j) < len(self._data)
+    def has_right(self, j):
+        return self.right(j) < len(self.data)
 
-    def _swap(self, i, j):
-        self._data[i], self._data[j] = self._data[j], self._data[i]
+    def swap(self, i, j):
+        self.data[i], self.data[j] = self.data[j], self.data[i]
 
-    def _upheap(self, j):
-        parent = self._parent(j)
-        if j > 0 and self._data[j] < self._data[parent]:
-            self._swap(j, parent)
-            self._upheap(parent)
+    def upheap(self, j): # 'bubbling up'
+        parent = self.parent(j)
+        if j > 0 and self.data[j] < self.data[parent]:
+            # j is not the parent and value is smaller than parent value
+            self.swap(j, parent) #call upon '.swap'
+            self.upheap(parent) #recursively call itself until heap order is met
 
-    def _downheap(self, j):
-        if self._has_left(j):
-            left = self._left(j)
-            small_child = left
-            if self._has_right(j):
-                right = self._right(j)
-                if self._data[right] < self._data[left]:
-                    small_child = right
-            if self._data[small_child] < self._data[j]:
-                self._swap(j, small_child)
-                self._downheap(small_child)
+    def downheap(self, j): # 'bubbling down'
+        if self.has_left(j): #check if left node is present
+            left = self.left(j) #value of left child = left
+            small_child = left # although right maybe smaller
+            if self.has_right(j): #check if right child is present
+                right = self.right(j)
+                if self.data[right] < self.data[left]: #if right is smaller
+                    small_child = right # assign right as the smaller child
+            if self.data[small_child] < self.data[j]: # if the value of small child is smaller than 'j'
+                self.swap(j, small_child)  #swap values
+                self.downheap(small_child) #recursively call itself until heap order is met
 
     '''PUBLIC BEHAVIOURS'''
 
-    def __init(self):
-        self._data = []
+    def __init__(self):
+        self.data = []
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
     def add(self, key, value):
-        self._data.append(self._item(key, value))
-        self._upheap(len(self._data) - 1)
+        self.data.append(self.Item(key, value))
+        self.upheap(len(self.data) - 1)
 
     def min(self):
         if self.is_empty():
             raise Exception('Priority Queue is empty')
-        item = self._data[0]
-        return(item._key, item._value)
+        item = self.data[0] #root node
+        return(item.key, item.value)
 
     def remove_min(self):
         if self.is_empty():
             raise Exception('Priority Queue is empty')
-        self._swap(0, len(self._data) - 1)
-        item = self._data.pop()
-        self._downheap(0)
-        return (item._key, item._value)
+        self.swap(0, len(self.data) - 1)
+        item = self.data.pop()
+        self.downheap(0)
+        return (item.key, item.value)
 
 
-class AdaptiveHeapPriorityQueue(HeapPriorityQueue):
-    class Locator(HeapPriorityQueue._item):
-        __slots__ = '_index'
+class AdaptableHeapPriorityQueue(HeapPriorityQueue): #subclass of HeapPriorityQueue
+    '''A locator-based priority queue implemented with a binary heap'''
+    class Locator(HeapPriorityQueue.Item):
+        __slots__ = '_index' #Adding index as an initial field
 
         def __init__(self, k, v, j):
-            super()._init__(k,v)
+            super().__init__(k,v)
             self._index = j
 
-    def _swap(self, i, j):
-        super().__swap(i, j)
-        self._data[i].__index = i
-        self._data[j]._index = j
+    def swap(self, i, j):
+        super().swap(i, j)
+        self.data[i]._index = i
+        self.data[j]._index = j
 
-    def _bubble(self, j):
-        if j > 0 and self._data[j] < self._data[self._parent(j)]:
-            self._uphead(j)
+    def bubble(self, j):
+        if j > 0 and self.data[j] < self.data[self.parent(j)]:
+            self.upheap(j)
         else:
-            self._downheap(j)
+            self.downheap(j)
 
     def add(self, key, value):
-        token = self.Locator(key, value, len(self._data))
-        self._data.append(token)
-        self._upheap(len(self._data) - 1)
+        token = self.Locator(key, value, len(self.data))
+        self.data.append(token)
+        self.upheap(len(self.data) - 1)
         return token
 
     def update(self, loc, newkey, newval):
         j = loc._index
-        if not (0 <= j < len(self) and self._data[j] is loc):
+        if not (0 <= j < len(self) and self.data[j] is loc):
             raise ValueError("Invalid Locator")
-        loc._key = newkey
-        loc._value = newval
-        self._bubble(j)
+        loc.key = newkey
+        loc.value = newval
+        self.bubble(j)
 
     def remove(self, loc):
         j = loc._index
-        if not (0 <= j < len(self) and self._data[j] is loc):
+        if not (0 <= j < len(self) and self.data[j] is loc):
             raise ValueError("Invalid Locator")
         if j == len(self) - 1:
-            self._data.pop()
+            self.data.pop()
         else:
-            self._swap(j, len(self)-1)
-            self._data.pop()
-            self._bubble(j)
-        return(loc._key, loc._value)
+            self.swap(j, len(self) - 1)
+            self.data.pop()
+            self.bubble(j)
+        return(loc.key, loc.value)
